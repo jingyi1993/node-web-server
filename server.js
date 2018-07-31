07ca3ca5e41addffad6caa1c2e0d436fd499a808
马静怡 <472382917@qq.com>
2018-07-31 16:09:00 -0400

    heroku

--- server.js
+++ server.js
@@ -1,20 +1,53 @@
 const express = require('express');
 //handlebars;
 const hbs = require('hbs');
+const fs =require('fs');
+const port = process.env.PORT || 3000;
+
+
 
 var app = express();
+
+hbs.registerPartials(__dirname +'/views/partials');
+
+//1st argument the functionName, 2nd argument the function;
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getTime();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+
+})
 
 app.set('view engine:','hbs');
 
 app.use(express.static(__dirname + '/public'));
 // console.log(__dirname);
+//middleware
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    // console.log(`${now}:${req.method},${req.url}`);
+    var log= `${now}: ${req.method}, ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log +'\n', (e)=>{
+        if(e){
+            console.log('unable to append file!!!')
+        }
+    });
+        next();
+});
+//middleware
+app.use((req,res,next)=>{
+    res.render('maintance.hbs');
+    next();
+})
 
 app.get('/', (req, res )=>{
     // res.send('<h1>hihihihi</h1>')
     res.render('home.hbs',
         {pageTitle: 'home page',
             welcomeMesssage: 'welcome to this page',
-            currentYear: new Date().getFullYear(),
         }
     )
 });
@@ -25,7 +58,6 @@ app.get('/about', (req,res)=>{
     res.render('about.hbs', {
         pageTitle:'about page',
         //return year
-        currentYear: new Date().getFullYear(),
 
     });
 });
@@ -36,6 +68,6 @@ app.get('/bad', (req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('server is on on port 3000')
+app.listen(port,()=>{
+    console.log(`server is on port ${port}`)
 });
